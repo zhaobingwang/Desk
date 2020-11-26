9bbe6a058ec98d13c053d8480c56fe56b1677da1
zhaobingwang <zhaobingwang@foxmail.com>
2020-11-26 17:26:37 +0800

    临时提交：ToLookup

---
+++ Gist/src/Desk.Gist.Test.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Desk.Gist.API.System.Linq.Enumerable;
+using System;
+
+namespace Desk.Gist.Test.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToLookUpGist.ToLookupGist();
+        }
+    }
+}
