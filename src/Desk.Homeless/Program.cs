f47d60373b4e1917078bed867694a55a441adf11
zhaobingwang <zhaobingwang@foxmail.com>
2020-11-26 12:22:14 +0800

    initialize project

---
+++ src/Desk.Homeless/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desk.Homeless
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
