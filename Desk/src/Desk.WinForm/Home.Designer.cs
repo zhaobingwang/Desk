4caeab711ab268bbda8e5cb7194c996272ccf571
zhaobingwang <zhaobingwang@foxmail.com>
2020-12-15 17:28:28 +0800

    feat: 支持托盘栏显示及托盘栏右键操作

--- Desk/src/Desk.WinForm/Home.Designer.cs
+++ Desk/src/Desk.WinForm/Home.Designer.cs
@@ -29,10 +29,15 @@ namespace Desk.WinForm
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.mnsHome = new System.Windows.Forms.MenuStrip();
             this.mnsItemTools = new System.Windows.Forms.ToolStripMenuItem();
             this.mnsItemAsset = new System.Windows.Forms.ToolStripMenuItem();
+            this.icnMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmnNotify = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmnNotifyExit = new System.Windows.Forms.ToolStripMenuItem();
             this.mnsHome.SuspendLayout();
+            this.cmnNotify.SuspendLayout();
             this.SuspendLayout();
             // 
             // mnsHome
@@ -42,7 +47,8 @@ namespace Desk.WinForm
             this.mnsItemTools});
             this.mnsHome.Location = new System.Drawing.Point(0, 0);
             this.mnsHome.Name = "mnsHome";
-            this.mnsHome.Size = new System.Drawing.Size(800, 28);
+            this.mnsHome.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.mnsHome.Size = new System.Drawing.Size(978, 32);
             this.mnsHome.TabIndex = 0;
             this.mnsHome.Text = "mnsHome";
             // 
@@ -51,27 +57,51 @@ namespace Desk.WinForm
             this.mnsItemTools.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnsItemAsset});
             this.mnsItemTools.Name = "mnsItemTools";
-            this.mnsItemTools.Size = new System.Drawing.Size(63, 24);
+            this.mnsItemTools.Size = new System.Drawing.Size(71, 28);
             this.mnsItemTools.Text = "Tools";
             // 
             // mnsItemAsset
             // 
             this.mnsItemAsset.Name = "mnsItemAsset";
-            this.mnsItemAsset.Size = new System.Drawing.Size(139, 26);
+            this.mnsItemAsset.Size = new System.Drawing.Size(164, 34);
             this.mnsItemAsset.Text = "Assets";
+            // 
+            // icnMain
+            // 
+            this.icnMain.ContextMenuStrip = this.cmnNotify;
+            this.icnMain.Text = "notifyIcon1";
+            this.icnMain.Visible = true;
+            this.icnMain.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.icnMain_MouseDoubleClick);
+            // 
+            // cmnNotify
+            // 
+            this.cmnNotify.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.cmnNotify.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmnNotifyExit});
+            this.cmnNotify.Name = "cmnNotify";
+            this.cmnNotify.Size = new System.Drawing.Size(117, 34);
+            // 
+            // cmnNotifyExit
+            // 
+            this.cmnNotifyExit.Name = "cmnNotifyExit";
+            this.cmnNotifyExit.Size = new System.Drawing.Size(116, 30);
+            this.cmnNotifyExit.Text = "退出";
             // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(978, 540);
             this.Controls.Add(this.mnsHome);
             this.MainMenuStrip = this.mnsHome;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Home";
             this.Text = "Home";
             this.Load += new System.EventHandler(this.Home_Load);
+            this.SizeChanged += new System.EventHandler(this.Home_SizeChanged);
             this.mnsHome.ResumeLayout(false);
             this.mnsHome.PerformLayout();
+            this.cmnNotify.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +112,9 @@ namespace Desk.WinForm
         private System.Windows.Forms.MenuStrip mnsHome;
         private System.Windows.Forms.ToolStripMenuItem mnsItemTools;
         private System.Windows.Forms.ToolStripMenuItem mnsItemAsset;
+        private System.Windows.Forms.NotifyIcon icnMain;
+        private System.Windows.Forms.ContextMenuStrip cmnNotify;
+        private System.Windows.Forms.ToolStripMenuItem cmnNotifyExit;
     }
 }
 
--- Desk/src/Desk.WinForm/Home.cs
+++ Desk/src/Desk.WinForm/Home.cs
@@ -23,6 +23,12 @@ namespace Desk.WinForm
         private void BindEvent()
         {
             mnsItemAsset.Click += MnsItemAsset_Click;
+            cmnNotifyExit.Click += CmnNotifyExit_Click;
+        }
+
+        private void CmnNotifyExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
 
         private void MnsItemAsset_Click(object sender, EventArgs e)
@@ -33,6 +39,33 @@ namespace Desk.WinForm
 
         private void Home_Load(object sender, EventArgs e)
         {
+        }
+
+        private void Home_SizeChanged(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                // 隐藏任务栏图标
+                ShowInTaskbar = false;
+                // 图标显示在托盘栏
+                icnMain.Icon = SystemIcons.Application;
+                icnMain.Visible = true;
+            }
+        }
+
+        private void icnMain_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                // 还原窗体显示    
+                WindowState = FormWindowState.Normal;
+                // 激活窗体并给予它焦点
+                Activate();
+                // 任务栏区显示图标
+                ShowInTaskbar = true;
+                // 托盘区图标隐藏
+                icnMain.Visible = false;
+            }
         }
     }
 }
