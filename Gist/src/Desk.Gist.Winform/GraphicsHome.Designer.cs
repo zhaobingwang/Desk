8fd0ba683dfe3ee94f79a6f3ad75fe6b3fcb866e
zhaobingwang <zhaobingwang@foxmail.com>
2020-12-18 17:18:45 +0800

    feat: 新增Desk.Gist.Winform及绘图demo

---
+++ Gist/src/Desk.Gist.Winform/GraphicsHome.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Desk.Gist.Winform
+{
+    partial class GraphicsHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnlCanvas
+            // 
+            this.pnlCanvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCanvas.Location = new System.Drawing.Point(446, 12);
+            this.pnlCanvas.Name = "pnlCanvas";
+            this.pnlCanvas.Size = new System.Drawing.Size(760, 721);
+            this.pnlCanvas.TabIndex = 0;
+            this.pnlCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlCanvas_Paint);
+            this.pnlCanvas.Resize += new System.EventHandler(this.pnlCanvas_Resize);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            // 
+            // GraphicsHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1218, 745);
+            this.Controls.Add(this.pnlCanvas);
+            this.Name = "GraphicsHome";
+            this.Text = "GraphicsHome";
+            this.Load += new System.EventHandler(this.GraphicsHome_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCanvas;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Gist/src/Desk.Gist.Winform/GraphicsHome.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Desk.Gist.Winform
+{
+    public partial class GraphicsHome : Form
+    {
+        System.Timers.Timer timer;
+        private int maxX;
+        private int maxY;
+        public GraphicsHome()
+        {
+            InitializeComponent();
+        }
+
+
+        private void GraphicsHome_Load(object sender, EventArgs e)
+        {
+            maxX = pnlCanvas.Width;
+            maxY = pnlCanvas.Height;
+        }
+
+        public void TimeOut(object source, System.Timers.ElapsedEventArgs e)
+        {
+            Draw2();
+        }
+
+        private void pnlCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            //timer = new System.Timers.Timer();
+            //timer.Interval = 3000;
+            //timer.Elapsed += new System.Timers.ElapsedEventHandler(TimeOut);
+            //timer.AutoReset = true;
+            //timer.Enabled = true;
+
+            timer1.Interval = 3000;
+            timer1.Enabled = true;
+            timer1.Tick += Timer1_Tick;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Draw2();
+        }
+
+        private void Draw()
+        {
+            Graphics g = pnlCanvas.CreateGraphics();
+            Pen pen = new Pen(Color.Red);
+            Point p1 = new Point(new Random().Next(maxX), new Random().Next(maxY));
+            Point p2 = new Point(new Random().Next(maxX), new Random().Next(maxY));
+            g.DrawLine(pen, p1, p2);
+        }
+
+        private void Draw2()
+        {
+            Point p = new Point(new Random().Next(maxX), new Random().Next(maxY));
+            var random = new Random().Next(5, 50);
+            Size size = new Size(random, random);
+
+            SolidBrush myBrush = new SolidBrush(Color.FromArgb(new Random().Next(0, 255), new Random().Next(0, 255), new Random().Next(0, 255)));
+            Graphics formGraphics;
+            formGraphics = pnlCanvas.CreateGraphics();
+            formGraphics.FillEllipse(myBrush, new Rectangle(p, size));
+            myBrush.Dispose();
+            formGraphics.Dispose();
+        }
+
+        private void pnlCanvas_Resize(object sender, EventArgs e)
+        {
+            maxX = pnlCanvas.Width;
+            maxY = pnlCanvas.Height;
+        }
+    }
+}
