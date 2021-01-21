3f7600d1f828e5f7156c765ee15d5b1fdf51525b
zhaobingwang <zhaobingwang@foxmail.com>
2021-01-21 16:15:41 +0800

    feat: graphics and multimedia in WPF

---
+++ Gist/src/Desk.Gist.WPF/GraphicsAndMultimedia/Graphics/LineGist.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desk.Gist.WPF.GraphicsAndMultimedia.Graphics
+{
+    /// <summary>
+    /// Interaction logic for LineGist.xaml
+    /// </summary>
+    public partial class LineGist : Window
+    {
+        public LineGist()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gist/src/Desk.Gist.WPF/GraphicsAndMultimedia/Graphics/RectangleGist.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desk.Gist.WPF.GraphicsAndMultimedia.Graphics
+{
+    /// <summary>
+    /// Interaction logic for RectangleGist.xaml
+    /// </summary>
+    public partial class RectangleGist : Window
+    {
+        public RectangleGist()
+        {
+            InitializeComponent();
+        }
+    }
+}
