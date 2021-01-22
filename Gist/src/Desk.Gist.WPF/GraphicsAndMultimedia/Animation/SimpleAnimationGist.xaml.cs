057cb5826fe73ea4b980abc8f7f47b7536902f4c
zhaobingwang <zhaobingwang@foxmail.com>
2021-01-22 16:34:38 +0800

    feat: add Simple Animation Gist

---
+++ Gist/src/Desk.Gist.WPF/GraphicsAndMultimedia/Animation/SimpleAnimationGist.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desk.Gist.WPF.GraphicsAndMultimedia.Animation
+{
+    /// <summary>
+    /// Interaction logic for SimpleAnimationGist.xaml
+    /// </summary>
+    public partial class SimpleAnimationGist : Window
+    {
+        public SimpleAnimationGist()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMove_Click(object sender, RoutedEventArgs e)
+        {
+
+            DoubleAnimation daX = new DoubleAnimation();
+            DoubleAnimation daY = new DoubleAnimation();
+
+            daX.From = 0;
+            daY.From = 0;
+
+            var rnd = new Random();
+            daX.To = rnd.NextDouble() * (gridMain.Width - ellipse.Width);
+            daY.To = rnd.NextDouble() * (gridMain.Height - ellipse.Height);
+
+            Duration duration = new Duration(TimeSpan.FromMilliseconds(100));
+            daX.Duration = duration;
+            daY.Duration = duration;
+
+            tt.BeginAnimation(TranslateTransform.XProperty, daX);
+            tt.BeginAnimation(TranslateTransform.YProperty, daY);
+        }
+    }
+}
