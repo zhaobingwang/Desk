f3af88d92ab1f77395406cca3818fcb6672edd09
zhaobingwang <zhaobingwang@foxmail.com>
2021-03-12 16:45:27 +0800

    feat: Vue(components临时提交）

--- Vue/geektime-learning/vue-learning-app/src/App.vue
+++ Vue/geektime-learning/vue-learning-app/src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ msg }}
+    <div>
+      <input type="text" v-model="info" />
+      <button @click="handleClick">添加</button>
+    </div>
+    <ul>
+      <todo-item v-for="item in list" :key="item" :item="item"></todo-item>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import TodoItem from "./components/TodoItem.vue";
 
 export default {
-  name: 'App',
+  name: "app",
+  data() {
+    return {
+      msg: "Hello Vue",
+      info: "",
+      list: [],
+    };
+  },
+  methods: {
+    handleClick() {
+      this.list.push(this.info);
+      this.info = "";
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    TodoItem,
+  },
+};
 </script>
 
 <style>
---
+++ Vue/geektime-learning/vue-learning-app/src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="item">{{ item }}</li>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+
+<style>
+.item {
+  color: red;
+}
+</style>
