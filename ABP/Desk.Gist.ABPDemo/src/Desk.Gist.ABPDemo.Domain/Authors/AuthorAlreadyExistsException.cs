6dd9057dacb916451e998b2455ab6e57df168cf9
zhaobingwang <zhaobingwang@foxmail.com>
2021-03-17 09:45:02 +0800

    feat: abp demo(authors domain layer)

---
+++ ABP/Desk.Gist.ABPDemo/src/Desk.Gist.ABPDemo.Domain/Authors/AuthorAlreadyExistsException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace Desk.Gist.ABPDemo.Authors
+{
+    public class AuthorAlreadyExistsException : BusinessException
+    {
+        public AuthorAlreadyExistsException(string name) : base(ABPDemoDomainErrorCodes.AuthorAlreadyExists)
+        {
+            WithData("name", name);
+        }
+    }
+}
